9d563aa6c09dabd4793604ca46c773a3ee91d6d6
Daniil Sergeev <daniil.scorpio@gmail.com>
2023-11-21 11:39:38 +0300

    fix: fix problems

---
+++ produce-track/src/components/pages/commons/OrderForm/OrderForm.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../../../Button/Button";
+import Input from "../Input/Input";
+
+import classes from "./OrderForm.css";
+
+const OrderForm = ({ children, orderData, submitHandler }) => {
+    const [order, setOrder] = useState(
+        orderData
+            ? orderData
+            : {
+                productType: "",
+                quantity: 0,
+                customerName: "",
+                status: "",
+              }
+    );
+
+    const router = useNavigate();
+
+    const submit = (e) => {
+        e.preventDefault();
+        submitHandler(order);
+        setOrder({
+            productType: "",
+            quantity: 0,
+            customerName: "",
+            status: "",
+        });
+
+        router("/orders");
+    };
+
+    return (
+        <form className={classes.form}>
+            <h2 className={classes.formHeader}>{children} order</h2>
+            <div className={classes.inputs}>
+                <Input
+                    type="text"
+                    placeholder="Product Type"
+                    value={order.productType}
+                    onChange={(e) =>
+                        setOrder({ ...order, productType: e.target.value })
+                    }
+                />
+                <Input
+                    type="number"
+                    placeholder="Quantity"
+                    value={order.quantity}
+                    onChange={(e) =>
+                        setOrder({ ...order, quantity: parseInt(e.target.value) })
+                    }
+                />
+                <Input
+                    type="text"
+                    placeholder="Customer Name"
+                    value={order.customerName}
+                    onChange={(e) =>
+                        setOrder({ ...order, customerName: e.target.value })
+                    }
+                />
+                <Input
+                    type="text"
+                    placeholder="Status"
+                    value={order.status}
+                    onChange={(e) =>
+                        setOrder({ ...order, status: e.target.value })
+                    }
+                />
+            </div>
+            <Button onClick={submit}>{children}</Button>
+        </form>
+    );
+};
+
+export default OrderForm;    
